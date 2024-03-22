637f190769c25175ec44ff6ababfecf0873c0470
Adityamule96 <aadimule111@.com>
2024-03-22 12:25:34 +0530

    http module

--- RunServer.js
+++ RunServer.js
@@ -1,20 +1,47 @@
 // 1. imports http module using require
 
-let http = require ("http")
-
-// 2.  server has created and handled Request   
-let server = http.createServer (handleReq) ;
+//let http = require ("http")
 
 // handlereq 
-function handleReq(req,resp){
-  console.log("welcome to browser request has come in !!!!!")
+//function handleReq(req,resp){
+  //console.log("welcome to browser request has come in !!!!!")
+ // resp.write("hello , welcome to nodejs server");
+ //  resp.end();
+ 
+ /*var body="Hello World/n";
+ var content_length=body.length;
+ resp.writeHead(200,{
+         'Content-Length': content_length ,
+         'Content-Type':  'text/plain'});
+  resp.end(body);       
 }
+*/
+// 2.  server has created and handled Request   
+// let server = http.createServer (handleReq) ;
 
 // server.listen(port,ip-address)
 // port will be greater than 1024
-// ip address => 127.0.0.1 /   "localhost"
+// ip address => 127.0.0.1  (or)   "localhost"
+
 // 3.  server has listen to this port and ip-address
-server.listen(3000,"127.0.0.1");
+// server.listen(3000,"127.0.0.1");
 
+// 1. include http module using require on RunServer .js file
 
+let http = require ("http")
 
+function handleReq(req,resp){
+   var d1=new Date();
+   
+ var body=" <html><body bgcolor='lightgreen'>";
+ body=body+"<h1>welcome to NodeJs , today is </h1> "+d1;
+ body=body+"</body></html>";
+ var content_length = body.length;
+ resp.writeHead(200,{
+         'Content-Length': content_length ,
+         'Content-Type':  'text/html'});
+  resp.end(body);       
+}
+
+let server = http.createServer (handleReq) ;
+server.listen(3001,"127.0.0.1");
